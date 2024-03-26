10aae39e34d80db8f1e4f30adfd195e4a93f0c34
udaygirish <einsteingirish@gmail.com>
2024-03-26 12:28:25 -0400

    Shift to lib

---
+++ main.py
@@ -0,0 +1,26 @@
+import os 
+import sys 
+import json
+import glob 
+import torch 
+from PIL import Image 
+import cv2 
+import numpy as np
+
+
+def single_image_pipeline(image_path):
+    
+    # Read image 
+    image = Image.open(image_path).convert("RGB")
+    
+def main():
+    # Function to process a image
+    # Get objects and save as JSON
+    
+    
+    
+    
+    
+
+if __name__ == '__main__':
+    main()
--- utilities/yolov8_seg.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO('yolov8n-seg.pt')  # load an official model
-
-# Predict with the model
-results = model('https://ultralytics.com/images/bus.jpg')  
